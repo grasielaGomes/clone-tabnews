97892801c79de4cb4ee5096e78060824292f63af
Grasiela Gomes <produtoradama@gmail.com>
2024-01-22 11:54:50 +0000

    test: ensure api /status is returning correctly

--- tests/integration/api/vi/status/get.test.js
+++
@@ -1,4 +0,0 @@
-test("Should return 200 when call localhost/api/v1/status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-});
