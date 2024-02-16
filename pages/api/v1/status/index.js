619a015215b58d7a8d438c3be76074f0b7639e89
Grasiela Gomes <produtoradama@gmail.com>
2024-02-16 14:33:41 +0000

    refactor: chnage openedConnections query

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,24 +1,29 @@
 import database from "infra/database";
 
 async function status(request, response) {
+  const databaseName = process.env.POSTGRES_DB;
   const updatedAt = new Date().toISOString();
   const queryDatabaseStatus = `
       SHOW server_version;
       SHOW max_connections;
-      SELECT COUNT(*) FROM pg_stat_activity;
     `;
 
-  const [resultVersion, resultMaxConnections, resultOpenedConnections] =
+  const [resultVersion, resultMaxConnections] =
     await database.query(queryDatabaseStatus);
 
+  const resultOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
   const pgVersionNumber = resultVersion.rows[0].server_version;
-  const maxConenction = resultMaxConnections.rows[0].max_connections;
-  const openedConnection = resultOpenedConnections.rows[0].count;
+  const maxConenctions = resultMaxConnections.rows[0].max_connections;
+  const openedConnections = resultOpenedConnections.rows[0].count;
 
   const databaseStatus = {
-    version: pgVersionNumber,
-    max_connections: parseInt(maxConenction),
-    opened_connections: parseInt(openedConnection),
+    version: parseInt(pgVersionNumber),
+    max_connections: parseInt(maxConenctions),
+    opened_connections: openedConnections,
   };
   response.status(200).json({
     updated_at: updatedAt,
